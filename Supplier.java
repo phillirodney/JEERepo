f72a093edf69a014444963218b58c71d0006bb87
CieranB <Cieran.L.Burke@Gmail.com>
2016-11-16 11:38:10 +0000

    Cieran

---
+++ Supplier.java
@@ -0,0 +1,18 @@
+
+Public class Supplier {
+
+
+Private int id;
+Private String name;
+
+public Supplier(){
+
+}
+
+public Supplier( int id, String name){
+	this.id = id;
+	this.name =name;
+}
+
+
+}
