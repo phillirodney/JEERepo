c3acbbb6d7c7477d45884db905589f006e686b4b
mgleghorn <marcus.gleghorn@googlemail.com>
2016-11-16 14:43:43 +0000

    image/stockorder javax changes

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -1,6 +1,15 @@
+import javax.persistence.*;
 
+@Entity
+@Table(name="Images")
 public class Image {
 	
+	@Id
+	@Column(name="Product ID", nullable=false, unique=true)
+	//@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int productID;
+	
+	@Column(name="Path")
 	private String path;
 
 	public Image(String path)
