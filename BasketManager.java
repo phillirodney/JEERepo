87bb025bb79d91553ba4352b06a6ac9511fdfba2
lewypark <lewypark@gmail.com>
2016-11-17 12:12:37 +0000

    basket

--- BasketManager.java
+++ BasketManager.java
@@ -3,8 +3,7 @@ public interface BasketManager extends Customer{
 	
 	//List<Products> basket = new ArrayList<Products>();
 
-	public Basket createBasket(int productId, String name, int quantity, long stockID, String stockRequired,
-			String stockReserved, String currentStock, Boolean porousware, int customerId);
+	public Basket createBasket(Basket basket);
 
 
 }
