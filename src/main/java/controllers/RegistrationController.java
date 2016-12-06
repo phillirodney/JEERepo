1151f1e35fc99344f65c1f94d97a1af5b0144701
CieranB <Cieran.L.Burke@Gmail.com>
2016-12-06 11:41:09 +0000

    RegistrationController

--- src/main/java/controllers/RegistrationController.java
+++ src/main/java/controllers/RegistrationController.java
@@ -4,75 +4,8 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-<<<<<<< HEAD
-/*
-import com.qac.oc.controllers.session.CurrentUser;
-import com.qac.oc.services.RegistrationService;
-import com.qac.oc.util.RegistrationError;
-*/
 
-/**
- * This controller handles the registration of new users on the website.
- * 
- * @author Marcus
- */
-
-@Named("register")
-@RequestScoped
-public class RegistrationController {
-	@Inject
-	private RegistrationService registrationService;
-	@Inject
-	private CurrentUser currentUser;
-	private String firstName = "";
-	private String lastName = "";
-	private String email = "";
-	private String password = "";
-	private String addressLine1 = "";
-	private String addressLine2 = "";
-	private String town = "";
-	private String postcode = "";
-	
-	/**
-	 * Register the user
-	 * 
-	 * @return Returns nbgardens if the user can register or register if the user cant.
-	 */
-	public String register() {
-		if(!firstName.isEmpty() && !lastName.isEmpty() && !addressLine1.isEmpty() && !town.isEmpty() && !postcode.isEmpty() && !email.isEmpty() && !password.isEmpty())
-			try {
-				if(addressLine2.isEmpty())
-					currentUser.setCustomer(registrationService.register(firstName, lastName, email, password, addressLine1, town, postcode));
-				else
-					currentUser.setCustomer(registrationService.register(firstName, lastName, email, password, addressLine1, addressLine2, town, postcode));
-				return "nbgardens";
-			} catch (RegistrationError re) {
-				System.out.println(re.getMessage());
-			}
-		return "register";
-	}
-
-	public String getFirstName() { return firstName; }
-	public String getLastName() { return lastName; }
-	public String getEmail() { return email; }
-	public String getPassword() { return password; }
-	public String getAddressLine1() { return addressLine1; }
-	public String getAddressLine2() { return addressLine2; }
-	public String getTown() { return town; }
-	public String getPostcode() { return postcode; }
-	
-	public void setFirstName(String firstName) { this.firstName = firstName; }
-	public void setLastName(String lastName) { this.lastName = lastName; }
-	public void setEmail(String email) { this.email = email; }
-	public void setPassword(String password) { this.password = password; }
-	public void setAddressLine1(String addressLine1) { this.addressLine1 = addressLine1; }
-	public void setAddressLine2(String addressLine2) { this.addressLine2 = addressLine2; }
-	public void setTown(String town) { this.town = town; }
-	public void setPostcode(String postcode) { this.postcode = postcode; }
-	
-}
-=======
-/* @author Cieran */ 
+/* @author Cieran & Marcus */ 
 
 @RequestScoped
 @Named(value = "registration")
@@ -85,6 +18,7 @@ public class RegistrationController {
 	@Inject 
 	private RegistrationService registrationService;
 	
+	// These four variables to login and view the gnomes, the rest can be added in the account page
 	private String name = "";
 	private String email = "";
 	private String phone = "";
@@ -108,9 +42,26 @@ public class RegistrationController {
 		this.phone = phone; }
 	public String setPassword(){
 		this.password = password; }
-	}
-
-
+	
+	
+	public String register () {
+		
+		if(!name.isEmpty() && !email.isEmpty() && !phone.isEmpty() && !password.isEmpty()) {
+			if (registrationService.validRegistration(name, email, phone, password)) {
+				currentUser.setCustomer(reigistrationService.registerUser(email));
+				return "Gnomepage2";
+			}
+			else {
+				name = "";
+				phone = "";
+				password = "";
+				return "Register";
+			}
+		}
+			name = "";
+			phone = "";
+			password = "";
+			return "Register"; 
 }
 
 >>>>>>> 4ae4b55f34aadc767ba826c1c6af9c0900770023
