0dea1c36dbff1b9032fe8fe53d7c22fab8f031ee
phillirodney <phillirodney@gmail.com>
2016-12-07 11:23:25 +0000

    increment functionality added

--- src/main/java/controllers/BasketController.java
+++ src/main/java/controllers/BasketController.java
@@ -51,6 +51,7 @@ public class BasketController {
 		
 	}
 	
+	
 	public void removeFromBasket() {
 		
 		
--- src/main/webapp/js/product.js
+++ src/main/webapp/js/product.js
@@ -18,3 +18,18 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+function decrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+}
