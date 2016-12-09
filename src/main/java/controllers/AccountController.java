5bb8999aa5779fb097e6c9f2203871067b40acff
phillirodney <phillirodney@gmail.com>
2016-12-09 09:43:01 +0000

    ArrayList<supplierorderline> added to stock order.java

--- src/main/java/controllers/AccountController.java
+++ src/main/java/controllers/AccountController.java
@@ -1,16 +1,18 @@
 package controllers;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 /*author Cieran*/
 
 @RequestScoped
-@Named (value = "account")
+@Named (value = "account ")
 
 public class AccountController {
 	
 	@Inject 
 	private CurrentUser currentUser;
 	
-	@Inject
-	private 
 
 }
