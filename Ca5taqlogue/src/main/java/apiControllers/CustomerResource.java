cbdacfbc460efc45ef53c7c7608b8b0d9cadf406
mgleghorn <marcus.gleghorn@googlemail.com>
2016-12-16 13:23:25 +0000

    dsa

--- Ca5taqlogue/src/main/java/apiControllers/CustomerResource.java
+++ Ca5taqlogue/src/main/java/apiControllers/CustomerResource.java
@@ -5,7 +5,10 @@ import java.sql.Date;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -19,7 +22,7 @@ import models.ProductManager;
 import services.LoginService;
 
 
-@Path("/customers")
+@Path("/")
 public class CustomerResource {
 	
 	@Inject
@@ -33,5 +36,18 @@ public class CustomerResource {
 		
 		return loginService.findById(id);
 	}
+	
+	@Path("login")
+	@POST
+	@Consumes({MediaType.APPLICATION_JSON})
+	public String login(@FormParam("email") String e, 
+			@FormParam("password") String p) {
+		
+		//loginService.loginUser(e);
+		return "Logged with " + e + " " + p;
+	}
+	
+	
+	
 
 }
